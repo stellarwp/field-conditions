04cccae54de095f5291dbb381ba706fd721184ee
Jason Adams <jason.adams@impress.org>
2023-02-06 15:45:12 -0800

    test: add SimpleConditionSet tests

---
+++ tests/unit/SimpleConditionSetTest.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+use StellarWP\FieldConditions\FieldCondition;
+use StellarWP\FieldConditions\SimpleConditionSet;
+use StellarWP\FieldConditions\Tests\TestCase;
+
+class SimpleConditionSetTest extends TestCase
+{
+    /**
+     * @unreleased
+     */
+    public function testShouldRunAndPassConditionsWithLogicalAnd()
+    {
+        $condition1 = $this->createMock(FieldCondition::class);
+        $condition1->method('passes')->with([])->willReturn(true);
+        $condition1->method('getLogicalOperator')->willReturn('and');
+
+        $condition2 = $this->createMock(FieldCondition::class);
+        $condition2->method('passes')->with([])->willReturn(true);
+        $condition2->method('getLogicalOperator')->willReturn('and');
+
+        $conditionSet = new SimpleConditionSet($condition1, $condition2);
+
+        self::assertTrue($conditionSet->passes([]));
+    }
+
+    /**
+     * @unreleased
+     */
+    public function testShouldRunAndPassConditionsWithLogicalOr()
+    {
+        $condition1 = $this->createMock(FieldCondition::class);
+        $condition1->method('passes')->with([])->willReturn(true);
+        $condition1->method('getLogicalOperator')->willReturn('or');
+
+        $condition2 = $this->createMock(FieldCondition::class);
+        $condition2->method('passes')->with([])->willReturn(false);
+        $condition2->method('getLogicalOperator')->willReturn('or');
+
+        $conditionSet = new SimpleConditionSet($condition1, $condition2);
+
+        self::assertTrue($conditionSet->passes([]));
+    }
+
+    /**
+     * @unreleased
+     */
+    public function testShouldRunAndFailConditionsWithLogicalAnd()
+    {
+        $condition1 = $this->createMock(FieldCondition::class);
+        $condition1->method('passes')->with([])->willReturn(true);
+        $condition1->method('getLogicalOperator')->willReturn('and');
+
+        $condition2 = $this->createMock(FieldCondition::class);
+        $condition2->method('passes')->with([])->willReturn(false);
+        $condition2->method('getLogicalOperator')->willReturn('and');
+
+        $conditionSet = new SimpleConditionSet($condition1, $condition2);
+
+        self::assertTrue($conditionSet->fails([]));
+    }
+
+    /**
+     * @unreleased
+     */
+    public function testShouldRunAndFailConditionsWithLogicalOr()
+    {
+        $condition1 = $this->createMock(FieldCondition::class);
+        $condition1->method('passes')->with([])->willReturn(false);
+        $condition1->method('getLogicalOperator')->willReturn('and');
+
+        $condition2 = $this->createMock(FieldCondition::class);
+        $condition2->method('passes')->with([])->willReturn(false);
+        $condition2->method('getLogicalOperator')->willReturn('or');
+
+        $conditionSet = new SimpleConditionSet($condition1, $condition2);
+
+        self::assertTrue($conditionSet->fails([]));
+    }
+
+    /**
+     * @unreleased
+     */
+    public function testShouldReturnConditions()
+    {
+        $condition = $this->createMock(FieldCondition::class);
+        $conditionSet = new SimpleConditionSet($condition);
+
+        $this->assertSame([$condition], $conditionSet->getConditions());
+    }
+
+    /**
+     * @unreleased
+     */
+    public function testShouldReturnSerializedConditions()
+    {
+        $condition = $this->createMock(FieldCondition::class);
+        $conditionSet = new SimpleConditionSet($condition);
+
+        $this->assertSame([$condition], $conditionSet->jsonSerialize());
+    }
+
+    /**
+     * @unreleased
+     */
+    public function testShouldAllowIterationOverConditions()
+    {
+        $condition = $this->createMock(FieldCondition::class);
+        $conditionSet = new SimpleConditionSet($condition);
+
+        foreach($conditionSet as $condition) {
+            self::assertInstanceOf(FieldCondition::class, $condition);
+        }
+    }
+}
